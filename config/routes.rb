d709dd7cee855989558543758a4de074cf04a2a6
sakitakech <ak1.2@icloud.com>
2025-03-28 02:13:29 +0900

    fix lint

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   resources :trainings
   devise_for :users
-  
+
   get "up" => "rails/health#show", as: :rails_health_check
 
 
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  root 'static_pages#top'
-
+  root "static_pages#top"
 end
