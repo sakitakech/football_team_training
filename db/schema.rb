b9d245a7b32637eba8f9c015ead5d946cce6674a
sakitakech <ak1.2@icloud.com>
2025-03-27 18:36:39 +0900

    add:user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_25_142948) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_27_093511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,9 +27,14 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_25_142948) do
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "name"
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
