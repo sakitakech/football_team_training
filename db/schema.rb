1ac25fdbfbdda8b6c56a98023155babff9e30176
sakitakech <ak1.2@icloud.com>
2025-03-27 18:58:04 +0900

    add:UserモデルとSessionモデルの関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_27_093511) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_27_095425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,4 +37,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_27_093511) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "sessions", "users"
 end
