bf2428b09bc37121d552f9df5fff4d72ead3b151
sakitakech <ak1.2@icloud.com>
2025-03-31 18:47:57 +0900

    change:signup,signin後のページをトレーニング一覧画面に変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -19,12 +19,10 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # protected
-  protected
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  def after_sign_in_path_for(resource)
-    trainings_path  # ← 任意のルートに変更可能
-  end
+
 end
