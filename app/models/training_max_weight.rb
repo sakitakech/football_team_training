ead2709c58dbb55df0ee5ba1cc8ed1a79106eb46
sakitakech <ak1.2@icloud.com>
2025-04-01 23:48:13 +0900

    fix:トレーニング数値空欄によるバグ回避

--- app/models/training_max_weight.rb
+++ app/models/training_max_weight.rb
@@ -1,6 +1,5 @@
 class TrainingMaxWeight < ApplicationRecord
     belongs_to :max_weight
     belongs_to :training
-    
-    validates :record, numericality: { less_than: 10_000 }
+    validates :record, numericality: { less_than: 10_000, allow_blank: true }
 end
