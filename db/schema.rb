cd02951c271f53c743206d9e3e8651c4b4ee761a
sakitakech <ak1.2@icloud.com>
2025-04-04 14:25:59 +0900

    add:positions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_01_171403) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_04_052136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_01_171403) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_max_weights_on_name"
+  end
+
+  create_table "positions", force: :cascade do |t|
+    t.string "name"
+    t.string "short_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "training_max_weights", force: :cascade do |t|
