c4ce70c643a8a9d9409c40f79ec080fa102f149c
sakitakech <ak1.2@icloud.com>
2025-04-10 01:48:42 +0900

    fix:アカウント削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -102,4 +102,16 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            'peperotion.oyama@gmail.com',
+    password:             'nlnb gkpb dtft xmzw',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
+
+    config.action_mailer.default_url_options = { host: 'gmail.com', protocol: 'https' }
 end
