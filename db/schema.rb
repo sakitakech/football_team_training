66aaa0c90e355e8d37762303542e9a6869bf4940
sakitakech <ak1.2@icloud.com>
2025-04-11 13:39:40 +0900

    add:leaguesテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_08_103936) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_11_042718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "leagues", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "max_weights", force: :cascade do |t|
     t.string "name"
