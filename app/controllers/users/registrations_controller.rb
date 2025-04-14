6f5829a8c7d7f4132fb68c4d716aa4ed66cc9d90
sakitakech <ak1.2@icloud.com>
2025-04-15 00:08:43 +0900

    change:ログインのエラー後の処理の変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -35,6 +35,35 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #     end
   #   end
   # end
+  def create
+    build_resource(sign_up_params)
+    # User.newと同じ
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+
+      # roleに応じてそれぞれのビューに戻す
+      case resource.role
+      when "admin"
+        render :new_admin, status: :unprocessable_entity
+      else
+        render :new_member, status: :unprocessable_entity
+      end
+    end
+  end
 
   # GET /resource/edit
   # def edit
