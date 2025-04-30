1bbe9d9f48be34ce8f0b35b3305f184058d0e8f1
sakitakech <ak1.2@icloud.com>
2025-04-30 15:27:36 +0900

    change：選手絞込み機能のUI

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,5 +2,10 @@ class CalendarController < ApplicationController
   def index
     @positions = Position.all
     @team_members = User.where(team_id: current_user.team_id).order(:last_name, :first_name)
+    @grouped_members = User
+     .includes(:position)
+     .where(team_id: current_user.team_id)
+     .order("positions.id ASC", "last_name ASC")
+     .group_by(&:position)
   end
 end
