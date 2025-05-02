782d470fcbf2639332b17699cdb76b19d92f2843
sakitakech <ak1.2@icloud.com>
2025-05-02 18:10:40 +0900

    change:teamに属してないユーザーのカレンダーとグラフの選択し非表示

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,6 +1,11 @@
 class ChartsController < ApplicationController
   def index
-    @team_members = current_user.team.users
+    @team_members =
+      if current_user.team_id.present?
+         User.where(team_id: current_user.team_id).order(:last_name, :first_name)
+      else
+        []
+      end
     @max_weights = MaxWeight.all
   end
 end
