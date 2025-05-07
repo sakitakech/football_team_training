3839d57a438172baf32d6d3ef1e11d7de978ddad
sakitakech <ak1.2@icloud.com>
2025-05-07 18:27:29 +0900

    add:メンバー選択、chartにもポジションを追加

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,11 +1,20 @@
 class ChartsController < ApplicationController
   def index
-    @team_members =
-      if current_user.team_id.present?
-         User.where(team_id: current_user.team_id).order(:last_name, :first_name)
-      else
-        []
-      end
+    if current_user.team_id.present?
+      @team_members = User.where(team_id: current_user.team_id).order(:last_name, :first_name)
+      @grouped_members = User
+        .includes(:position)
+        .where(team_id: current_user.team_id)
+        .order("positions.id ASC", "last_name ASC")
+        .group_by(&:position)
+    else
+      @team_members = []
+      @grouped_members = {}
+    end
+
     @max_weights = MaxWeight.all
+    @selected_user_id = params[:user_id].to_i if params[:user_id].present?
+
+    @user_position_map = @team_members.map { |u| [ u.id, u.position_id ] }.to_h
   end
 end
