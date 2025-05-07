6b0758bb4d9f3fe7f03fa79d9dce68c65ca0bf91
sakitakech <ak1.2@icloud.com>
2025-05-07 22:10:09 +0900

    add:joinrequestのnewmade

--- app/controllers/team_join_requests_controller.rb
+++ app/controllers/team_join_requests_controller.rb
@@ -1,5 +1,14 @@
 class TeamJoinRequestsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @token = params[:token]
+    if @token.blank?
+      redirect_to root_path, alert: "不正な招待URLです"
+    end
+
+    # いちいちDBにカラムを作らない簡単にオブジェクトを作る方法
+    @join_request = OpenStruct.new(message: "")
   end
 
   def create
