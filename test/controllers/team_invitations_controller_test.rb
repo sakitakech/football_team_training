30209a63ecd5c1a809016dbdc8ce2be9cf090bc8
sakitakech <ak1.2@icloud.com>
2025-05-15 19:24:17 +0900

    update:brakemanとbootsnap

--- test/controllers/team_invitations_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class TeamInvitationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get team_invitations_new_url
-    assert_response :success
-  end
-end
