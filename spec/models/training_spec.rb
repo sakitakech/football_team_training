dd0362b8fd1197b05cca4e6870db5b9df7e1f7e1
sakitakech <ak1.2@icloud.com>
2025-05-19 20:29:53 +0900

    add:テストファイル切り上げ、一旦チーム作成のみ

--- spec/models/training_spec.rb
+++ spec/models/training_spec.rb
@@ -1,14 +1,1 @@
-require 'rails_helper'
-
-RSpec.describe Training, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
-
-describe "クラス名やメソッドの処理内容などを記載する" do
-  context "状況や条件などを記載する" do
-    it "テストケースの説明" do
-      ...
-    end
-  end
-end
+#未作成
