b9424766498ff7ee6804e5a3c9cc08b11d3c41d6
sakitakech <ak1.2@icloud.com>
2025-05-20 00:23:59 +0900

    error:adminとmemberが分かれているため

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,30 @@ class User < ApplicationRecord
     ransacker :full_name do
       Arel.sql("CONCAT(users.last_name, users.first_name)")
     end
+
+
+    def social_profile(provider)
+      social_profiles.select { |sp| sp.provider == provider.to_s }.first
+    end
+  
+    def set_values(omniauth)
+      return if provider.to_s != omniauth["provider"].to_s || uid != omniauth["uid"]
+      credentials = omniauth["credentials"]
+      info = omniauth["info"]
+  
+      access_token = credentials["refresh_token"]
+      access_secret = credentials["secret"]
+      credentials = credentials.to_json
+      if info["name"].present?
+        full_name = info["name"]
+        self.last_name, self.first_name = full_name.split(" ", 2) # 名前を空白で分割
+        self.first_name ||= "未設定"  # 万が一分割できなかった時用
+        self.last_name  ||= "未設定"
+      end
+    end
+  
+    def set_values_by_raw_info(raw_info)
+      self.raw_info = raw_info.to_json
+      self.save!
+    end
 end
