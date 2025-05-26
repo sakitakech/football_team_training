b6d75374eaacc105f530fcdb9317f78c21a4857f
sakitakech <ak1.2@icloud.com>
2025-05-26 18:18:16 +0900

    chenge:メールの送信を1ヶ月ごとに

--- app/services/notification.rb
+++ app/services/notification.rb
@@ -2,7 +2,7 @@ class Notification
     def self.check_training_inactivity
       User.find_each do |user|
         last_training = user.trainings.order(datetime: :desc).first
-        if last_training.nil? || last_training.datetime < 1.week.ago
+        if last_training.nil? || last_training.datetime < 4.week.ago
           NotificationMailer.remind_user_to_train(user).deliver_now
         end
       end
