949339c0d60b74574115abffedb7a4966f89f291
sakitakech <ak1.2@icloud.com>
2025-05-26 17:38:43 +0900

    メール送信解決

--- app/services/notification.rb
+++ app/services/notification.rb
@@ -3,9 +3,7 @@ class Notification
       User.find_each do |user|
         last_training = user.trainings.order(datetime: :desc).first
         if last_training.nil? || last_training.datetime < 1.week.ago
-          puts "メール送信対象者確認: #{user.last_name}   #{user.email}（ID: #{user.id}）"
           NotificationMailer.remind_user_to_train(user).deliver_now
-          puts "送信済み"
         end
       end
     end
