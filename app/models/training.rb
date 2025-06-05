1cbed9e0892a78844c5cd9c35ec404ffd98a55df
sakitakech <ak1.2@icloud.com>
2025-06-05 16:46:50 +0900

    add:体重、体脂肪のバリデーション

--- app/models/training.rb
+++ app/models/training.rb
@@ -3,5 +3,10 @@ class Training < ApplicationRecord
   has_many :training_max_weights, dependent: :destroy
 
   validates :datetime, presence: true
+
+  validates :body_weight, numericality: { less_than: 1000, allow_blank: true }
+  validates :body_fat, numericality: { less_than: 100, allow_blank: true }
+
   accepts_nested_attributes_for :training_max_weights, allow_destroy: true
+
 end
